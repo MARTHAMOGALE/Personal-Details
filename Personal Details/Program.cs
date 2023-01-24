cd3edd24b75220df26c5f7bddc64321d4015ae05
MAROPENG MARTHA <104618684+MARTHAMOGALE@users.noreply.github.com>
2023-01-24 12:37:14 +0200

    Add project files.

---
+++ Personal Details/Program.cs
@@ -0,0 +1,42 @@
+﻿class Learners
+
+{
+    public string firstname;
+    public string lastname;
+    public string gender;
+    public int age;
+    public char initial;
+
+    public Learners (string Firstname,string Lastname, string Gender, int Age,char Initial)
+
+    {
+        firstname = Firstname;
+        lastname = Lastname;
+        gender = Gender;
+        age = Age;
+        initial = Initial;
+
+
+    }
+
+    static void Main (string [] args)
+    {
+        Learners learner1 = new Learners("Malesela", "Letsoalo", "Male", 30, 'J');
+
+        Console.WriteLine(learner1.firstname+ " " +learner1.lastname+ " " +learner1.gender+ " " +learner1.age+ " " +learner1.initial);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
